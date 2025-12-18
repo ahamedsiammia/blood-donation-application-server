da4a6e9b565b492a3e7f5b0c4473f6459247c4d2
ahamedsiammia <md7559263@gmail.com>
2025-12-18 15:47:25 +0600

    added new api

--- index.js
+++ index.js
@@ -314,6 +314,12 @@ async function run() {
       res.send(result)
     })
 
+    // donar dashboard api 
+    app.get("/resent-request",async(req,res)=>{
+      const result = await requestColocation.find().toArray
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
